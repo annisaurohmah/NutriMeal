706c25bb2145685fde5f6003703674ed3f9e5e36
LENOVO <annisaurohmah@mail.ugm.ac.id>
2024-01-12 07:55:57 +0700

    up

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,13 +12,13 @@
 <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
+        <div class="container-fluid ">
             <a class="navbar-brand" href="#">NutriMeal</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav me-auto">
+            <div class="collapse d-flex justify-content-end" id="navbarNavAltMarkup">
+                <div class="navbar-nav ">
                     <a class="nav-link" aria-current="page" href="#">Home</a>
                     <a class="nav-link" href="#">About</a>
                     <a class="nav-link" href="#">Foodpage</a>
@@ -36,30 +36,32 @@
     </div>
 
 
-    <div class="filterBox container-xxl mx-auto">
-        <form method="POST" enctype="multipart/form-data" class="d-flex align-items-end row g-4 p-3">
-            @csrf
-            <div class="col-md-2" style="flex: 0 0 20%;">
-                <label for="budget" class="form-label">Budget</label>
-                <select class="form-control" id="budget" name="budget" required>
-                </select>
-            </div>
-            <div class="col-md-2" style="flex: 0 0 20%;">
-                <label for="activity" class="form-label">Activity</label>
-                <input type="text" class="form-control" id="activity" placeholder="Tulis aktivitas.." name="activity" required>
-            </div>
-            <div class="col-md-2" style="flex: 0 0 20%;">
-                <label for="alergi" class="form-label">Alergi</label>
-                <input type="text" class="form-control" id="alergi" name="alergi">
-            </div>
-            <div class="col-md-2" style="flex: 0 0 20%;">
-                <label for="kandungan" class="form-label">Kandungan</label>
-                <input type="text" class="form-control" id="kandungan" name="kandungan">
-            </div>
-            <div class="col-md-2" style="flex: 0 0 20%;">
-                <button type="submit" class="btn btn-success" style="width: 100%">Search</button>
-            </div>
-        </form>
+    <div class="filter">
+        <div class="filterBox container-xxl mx-auto">
+            <form method="POST" enctype="multipart/form-data" class="d-flex align-items-end row g-4 p-3">
+                @csrf
+                <div class="col-md-2" style="flex: 0 0 20%;">
+                    <label for="budget" class="form-label">Budget</label>
+                    <select class="form-control" id="budget" name="budget" required>
+                    </select>
+                </div>
+                <div class="col-md-2" style="flex: 0 0 20%;">
+                    <label for="activity" class="form-label">Activity</label>
+                    <input type="text" class="form-control" id="activity" placeholder="Tulis aktivitas.." name="activity" required>
+                </div>
+                <div class="col-md-2" style="flex: 0 0 20%;">
+                    <label for="alergi" class="form-label">Alergi</label>
+                    <input type="text" class="form-control" id="alergi" name="alergi">
+                </div>
+                <div class="col-md-2" style="flex: 0 0 20%;">
+                    <label for="kandungan" class="form-label">Kandungan</label>
+                    <input type="text" class="form-control" id="kandungan" name="kandungan">
+                </div>
+                <div class="col-md-2" style="flex: 0 0 20%;">
+                    <button type="submit" class="btn btn-success" style="width: 100%">Search</button>
+                </div>
+            </form>
+        </div>
     </div>
 
     <div class="about">
